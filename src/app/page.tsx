3b31eeabbf7b267caaebf976d39782bf46943f78
blackboxai-deploy <deploy@blackboxai.dev>
2025-09-10 07:00:40 +0530

    Add src/app/page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { WebScraper } from '@/components/web-scraper';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen py-8 px-4 sm:px-6 lg:px-8">
+      <WebScraper />
+    </main>
+  );
+}
